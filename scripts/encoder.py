f266e121dba0e4e945412dd3781044333dd40c27
Kamil Kurach <k.a.kurach@gmail.com>
2024-07-26 22:56:57 +0200

    Update encoder test script

--- scripts/encoder.py
+++ scripts/encoder.py
@@ -1,9 +1,13 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
 
 while True:
-  state = GPIO.input(23)
-  print(state)
+  left_encoder = GPIO.input(23)
+  right_encoder = GPIO.input(24)
+  sleep(0.01)
+  print("L: ", left_encoder, "R: ", right_encoder)
 
