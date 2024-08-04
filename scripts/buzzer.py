2169c917e497595dcbb05017504eb2f213ede82b
Kamil Kurach <k.a.kurach@gmail.com>
2024-08-04 02:11:13 +0200

    Add buzzer test

---
+++ scripts/buzzer.py
@@ -0,0 +1,31 @@
+#buzzer test
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+
+def fast_beep():
+  GPIO.output(21, GPIO.HIGH)
+  sleep(0.05)
+  GPIO.output(21, GPIO.LOW)
+  sleep(0.05)
+
+def slow_beep():
+  GPIO.output(21, GPIO.HIGH)
+  sleep(0.1)
+  GPIO.output(21, GPIO.LOW)
+  sleep(0.1)
+
+count = 5
+
+while count > 0:
+  fast_beep()
+  count-=1
+
+count = 5
+
+while count > 0:
+  slow_beep()
+  count-=1
