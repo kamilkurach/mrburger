fd13efeea4b740fb8c8c672a9ed9c2e78e029435
Kamil Kurach <k.a.kurach@gmail.com>
2024-11-21 19:01:18 +0100

    Change for WS2812 led

--- scripts/led.py
+++ scripts/led.py
@@ -1,6 +1,7 @@
 from time import sleep
 import board
 import neopixel
+from buzzer import slow_beep, long_beep
 
 '''
 https://docs.circuitpython.org/projects/neopixel/en/latest/
@@ -9,53 +10,54 @@ sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 
 sudo python3 -m pip install --force-reinstall adafruit-blinka
 '''
-# LED Type: RGB WS2811 5mm
+# LED Type: RGB WS2812 SMD
 
 # GPIO10 SPI
 pixel_pin = board.D10
 num_pixels = 2
-order = neopixel.RGB
+order = neopixel.GRB
 
-led = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.95, auto_write=False, pixel_order=order)
+led = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.95, auto_write=True, pixel_order=order)
 
+# STATUS LED
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-color_tab = [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE]
+color_tab = [RED, YELLOW, GREEN, CYAN]
 
-right_led = 0
-left_led = 1
-
-# right LED
-for color in color_tab:
+# right & left turn signal (YELLOW)
+for i in range(2):
   for _ in range(3):
-    led[right_led] = color
+    led[i] = YELLOW
     led.show()
-    sleep(0.1)
-    led[right_led] = (0,0,0)
+    slow_beep()
+    sleep(0.3)
+    led[i] = BLACK
     led.show()
-    sleep(0.1)
+    sleep(0.3)
 
-# left LED
-for color in color_tab:
-  for _ in range(3):
-    led[left_led] = color
-    led.show()
-    sleep(0.1)
-    led[left_led] = (0,0,0)
-    led.show()
-    sleep(0.1)
+# stop light (RED)
 
-# both
-for color in color_tab:
-  for _ in range(3):
-    led.fill((color))
-    led.show()
-    sleep(0.1)
-    led.fill((0,0,0))
-    led.show()
-    sleep(0.1)
+for _ in range(2):
+  led.fill(RED)
+  led.show()
+  long_beep()
+  sleep(2)
+  led.fill(BLACK)
+  led.show()
+  sleep(2)
+
+# torch light (CYAN) for low light conditions
+
+led.fill(WHITE)
+led.show()
+sleep(3)
+led.fill(BLACK)
+led.show()
+
